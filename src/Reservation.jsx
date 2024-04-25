fe5260a8df91889ea00204b0f9ead51c556d7142
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-25 11:50:35 -0400

    got reservation form working, time to make everything look good/pretty

--- src/Reservation.jsx
+++ src/Reservation.jsx
@@ -1,8 +1,106 @@
+import React, { useState } from 'react';
+import './App.css';
+
 function Reservation() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        date: '',
+        time: '',
+        partySize: 1,
+        specialRequest: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Reservation Submitted:", formData);
+        alert("Reservation submitted successfully!");
+        setFormData({
+            name: '',
+            email: '',
+            date: '',
+            time: '',
+            partySize: 1,
+            specialRequest: ''
+        });
+    };
     return (
       <div>
-        Property of Jay Miller
-        3
+        <h2>Reservation Form</h2>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='name'>Name: </label>
+                <input 
+                  type='text'
+                  id='name'
+                  name='name'
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                  />
+            </div>
+            <div>
+                <label htmlFor='email'>Email: </label>
+                <input
+                  type='email'
+                  id='email'
+                  name='email'
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                  />
+            </div>
+            <div>
+                <label htmlFor='date'>Date: </label>
+                <input
+                  type='date'
+                  id='date'
+                  name='date'
+                  value={formData.date}
+                  onChange={handleChange}
+                  required
+                  />
+            </div>
+            <div>
+                <label htmlFor='time'>Time: </label>
+                <input
+                  type='time'
+                  id='time'
+                  name='time'
+                  value={formData.time}
+                  onChange={handleChange}
+                  required
+                  />
+            </div>
+            <div>
+                <label htmlFor='partySize'>PartySize: </label>
+                <input
+                  type='partySize'
+                  id='partySize'
+                  name='partySize'
+                  value={formData.partySize}
+                  onChange={handleChange}
+                  required
+                  />
+            </div>
+            <div>
+                <label htmlFor='specialRequest'>SpecialRequest: </label>
+                <input
+                  type='specialRequest'
+                  id='specialRequest'
+                  name='specialRequest'
+                  value={formData.specialRequest}
+                  onChange={handleChange}
+                  required
+                  />
+            </div>
+            <button className='text-light' type='submit'>Submit Reservation</button>
+        </form>
       </div>
     )
   }
