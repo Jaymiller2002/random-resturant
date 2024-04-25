c57e12be527654cb78a07038ae7e2ba2452246f8
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-25 15:29:30 -0400

    changed contact us to pop out more to the eye

--- src/About.jsx
+++ src/About.jsx
@@ -2,18 +2,15 @@
 function About() {
   return (
     <div className="p-5 center-card">
-      <div className="card">
-      <h1>Contact Us</h1>
-      <p>Phone Number: +1 (859) 475-8431</p>
-      <p>Hours: 8am-8pm</p>
-      <div></div>
-      <p>Open: Monday thru Saturday</p>
-      <div></div>
-      <p>Address: 3313 Pastern Court, Lexington, KY 40513</p>
+      <div className="card about-card">
+        <h1 className="text-3xl font-bold mb-3">Contact Us</h1>
+        <p className="text-lg mb-2">Phone Number: +1 (859) 475-8431</p>
+        <p className="text-lg mb-2">Hours: 8am-8pm</p>
+        <p className="text-lg mb-2">Open: Monday thru Saturday</p>
+        <p className="text-lg mb-2">Address: 3313 Pastern Court, Lexington, KY 40513</p>
       </div>
     </div>
-  )
+  );
 }
 
-
-export default About
+export default About;
